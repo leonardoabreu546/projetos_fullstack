422a6399be7f64d473c3bbd70ee01100e63e7b10
Leonardo <lcat140507@gmail.com>
2026-08-08 21:03:42 +0100

    adicionar estado do jogo

--- jogo_do_galo/frontend/src/pages/JogoPage.tsx
+++ jogo_do_galo/frontend/src/pages/JogoPage.tsx
@@ -1,13 +1,27 @@
-import { Square } from '../components/Square';
+import { useState } from 'react'
+import {Board} from '../components/Board'
 
 export function JogoPage() {
+  const [squares, setSquares] = useState<(string | null)[]>(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState<boolean>(true);
+
+  function handleClick(i: number) {
+    if (squares[i]) return;
+
+    const nextSquares = squares.slice();
+
+    nextSquares[i] = xIsNext ? 'X' : 'O';
+
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
   return (
-    <div className="container mt-5 text-center">
-      <h2>Testando o Componente Square</h2>
-      <div className="mt-4">
-        <Square value="X" onSquareClick={() => alert('Clicaste no X!')} />
-        <Square value="O" onSquareClick={() => alert('Clicaste no O!')} />
-      </div>
+    <div className="d-flex flex-column align-items-center">
+      <h1 className="mb-4">Jogo do Galo</h1>
+        <p>"Próximo a jogar:" <strong>{xIsNext ? 'X' : 'O'}</strong></p>
+        <Board squares={squares} onSquareClick={handleClick} />
     </div>
   );
 }
+  
