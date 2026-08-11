7977455331723f05e98d085b0220fdd6f3a43e46
Leonardo <lcat140507@gmail.com>
2026-08-11 23:05:08 +0100

    integrar sqlite no servidor

--- task_flow/backend/src/server.ts
+++ task_flow/backend/src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import cors from 'cors';
+import { conectarDB } from './database/index.js';
 
 const app = express();
 const PORT = 3333;
+conectarDB();
 
 // Middlewares
 app.use(cors());
